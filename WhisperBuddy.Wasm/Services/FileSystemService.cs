8dad8b8f38c7e4647bc8556410f02145b068dbc1
Germán De Francesco <germandefrancesco@hotmail.com>
2023-04-25 23:19:00 -0300

    Fix wasm missing service

---
+++ WhisperBuddy.Wasm/Services/FileSystemService.cs
@@ -0,0 +1,11 @@
+﻿using WhisperBuddy.Services;
+
+namespace WhisperBuddy.Wasm.Services;
+
+public class FileSystemService : IFileSystemService
+{
+    public string GetAppDataDirectory()
+    {
+        return Path.GetTempPath();
+    }
+}
