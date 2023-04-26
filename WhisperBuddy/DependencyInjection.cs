8a4edcfae87147c64c9371580ef8af3fc5acbaba
Germán De Francesco <germandefrancesco@hotmail.com>
2023-04-25 23:13:18 -0300

    Remove bootstrap and unused pages

--- WhisperBuddy/DependencyInjection.cs
+++ WhisperBuddy/DependencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
-using WhisperBuddy.Data;
 
 namespace WhisperBuddy;
 
@@ -9,6 +8,5 @@ public static class DependencyInjection
     public static void AddWhisperBuddyServices(this IServiceCollection services)
     {
         services.AddMudServices();
-        services.AddSingleton<WeatherForecastService>();
     }
 }
